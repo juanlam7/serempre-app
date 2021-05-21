b5789205171ffe69ad50f0466013f0e6f9261420
juanlam7 <juanlam7@gmail.com>
2021-05-21 16:45:07 -0400

    Estilos y tamaños de letra

---
+++ src/app/shared/data.ts
@@ -0,0 +1,66 @@
+export class Data {
+    public allData: any = [
+    {
+      titulo:'MOMENTUM True Wireless 2', 
+      subtitulo:'Earbuds that put sound first', 
+      price: '$295.95',
+      overview: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      types: [
+          {
+              titulo: 'Ivory White',
+              descripcion: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry'
+          },
+          {
+              titulo: 'Matte Black',
+              descripcion: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry'
+          }
+      ],
+      warranty: [
+        {
+            titulo: '2 years coverage',
+            descripcion: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry'
+        },
+        {
+            titulo: '3 years coverage',
+            descripcion: 'Lorem Ipsum is simply dummy text of the printing',
+            cost: '$25'
+        },
+      ],
+      features: [
+        {
+            titulo: 'Voice assistant support',
+        },
+        {
+            titulo: 'Customizable controls',
+            cost: '$25'
+        },
+      ],
+      specifications: [
+        {
+            item: 'Dimensions',
+            descripcion: 'Lorem Ipsum is simply'
+        },
+        {
+            item: 'USB Standard',
+            descripcion: 'USB-C'
+        },
+        {
+            item: 'Power supply',
+            descripcion: 'Lorem Ipsum is simply'
+        },
+        {
+            item: 'Frequency response (Microphone)',
+            descripcion: 'Lorem Ipsum is simply'
+        },
+        {
+            item: 'Frequency response',
+            descripcion: 'Lorem Ipsum is simply'
+        },
+        {
+            item: 'Noise cancellation',
+            descripcion: 'Lorem Ipsum is simply'
+        },
+      ],
+    }
+    ];
+  }
--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+import { Data } from '../../shared/data';
 
 @Component({
   selector: 'app-main',
@@ -10,9 +11,16 @@ export class MainComponent implements OnInit {
   public screenWidth: any;
   public screenHeight: any;
 
+  data: any;
+  product: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.data = new Data;
+    this.product = this.data.allData[0]
+    console.log(this.data)
+
     this.screenWidth = window.innerWidth;
     this.screenHeight = window.innerHeight;
   }
