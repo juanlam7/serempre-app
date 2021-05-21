b3c1454873ca7f88805b2fadb3274bba23622eae
juanlam7 <juanlam7@gmail.com>
2021-05-21 14:26:19 -0400

    Estructura defina para consumir datos

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  public screenWidth: any;
+  public screenHeight: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
   }
 
+  @HostListener('window:resize', ['$event'])
+
+  onResize(event: any) {
+    //event.target.innerWidth
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
+  }
+
+  calculateStyles() {
+    return { 'max-width': 'auto',  'max-height': `${this.screenHeight}px` };
+  }
 }
