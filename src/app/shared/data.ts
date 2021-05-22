fc7ea62149f7e8ed9c5fc91e8936cd9c03211d68
juanlam7 <juanlam7@gmail.com>
2021-05-21 20:34:26 -0400

    Estilos para item activos

--- src/app/shared/data.ts
+++ src/app/shared/data.ts
@@ -4,35 +4,57 @@ export class Data {
       titulo:'MOMENTUM True Wireless 2', 
       subtitulo:'Earbuds that put sound first', 
       price: '$295.95',
-      overview: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
       types: [
           {
               titulo: 'Ivory White',
-              descripcion: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry'
+              descripcion: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+              diff: false
           },
           {
               titulo: 'Matte Black',
-              descripcion: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry'
+              descripcion: 'Lorem Ipsum is simply dummy text of the printing',
+              diff: false
           }
       ],
       warranty: [
         {
             titulo: '2 years coverage',
-            descripcion: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry'
+            descripcion: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+            diff: false
         },
         {
             titulo: '3 years coverage',
             descripcion: 'Lorem Ipsum is simply dummy text of the printing',
-            cost: '$25'
+            cost: '$25',
+            diff: false
         },
+      ],
+      details: [
+        {
+            titulo: 'Overview',
+            descripcion: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+            diff: true
+        },
+        {
+            titulo: 'Features',
+            descripcion: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsu. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+            diff: false
+        },
+        {
+            titulo: "What's in the box?",
+            descripcion: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsu.",
+            diff: false
+        }
       ],
       features: [
         {
             titulo: 'Voice assistant support',
+            diff: false
         },
         {
             titulo: 'Customizable controls',
-            cost: '$25'
+            cost: '$25',
+            diff: false
         },
       ],
       specifications: [
--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -11,6 +11,9 @@ export class MainComponent implements OnInit {
   public screenWidth: any;
   public screenHeight: any;
 
+  defaulImg: any;
+  defaulDetail: any;
+
   data: any;
   product: any;
 
@@ -22,6 +25,9 @@ export class MainComponent implements OnInit {
 
     this.screenWidth = window.innerWidth;
     this.screenHeight = window.innerHeight;
+
+    this.defaulImg = './../../../assets/Sennheiser1.png';
+    this.defaulDetail = this.product.details[0].descripcion;
   }
 
   @HostListener('window:resize', ['$event'])
@@ -32,7 +38,58 @@ export class MainComponent implements OnInit {
     this.screenHeight = window.innerHeight;
   }
 
+  changePic(img: any) {
+    this.defaulImg = img;
+  }
+
+  changeDet(descripcion: any) {
+    this.defaulDetail = descripcion;
+
+    this.product.details.filter((asd: any) => {
+      if (asd.descripcion === descripcion) {
+        asd.diff = true
+      } else {
+        asd.diff = false
+      }
+    })
+
+  }
+
+  changeDet1(descripcion: any) {
+    this.product.types.filter((asd: any) => {
+      if (asd.descripcion === descripcion) {
+        asd.diff = true
+      } else {
+        asd.diff = false
+      }
+    })
+
+  }
+
+  changeDet2(descripcion: any) {
+    this.product.warranty.filter((asd: any) => {
+      if (asd.descripcion === descripcion) {
+        asd.diff = true
+      } else {
+        asd.diff = false
+      }
+    })
+
+  }
+
+  changeDet3(titulo: any) {
+    this.product.features.filter((asd: any) => {
+      if (asd.titulo === titulo) {
+        asd.diff = true
+      } else {
+        asd.diff = false
+      }
+    })
+
+  }
+
+
   calculateStyles() {
-    return { 'max-width': 'auto',  'max-height': `${this.screenHeight}px` };
+    return { 'max-width': 'auto', 'max-height': `${this.screenHeight}px` };
   }
 }
